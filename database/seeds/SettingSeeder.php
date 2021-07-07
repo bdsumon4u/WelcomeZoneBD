8e7ba009e2d354eb15c3d73fac5689fdf4ac6435
Sumon Ahmed <bdsumon4u@gmail.com>
2021-07-07 10:57:20 +0600

    SettingSeeder

--- database/seeds/SettingSeeder.php
+++ database/seeds/SettingSeeder.php
@@ -11,6 +11,46 @@ class SettingSeeder extends Seeder
      */
     public function run()
     {
-        //
+        App\Setting::truncate();
+        \App\Setting::create([
+            'name' => 'logo',
+            'value' => [
+                'favicon' => '',
+                'desktop' => '',
+                'mobile' => '',
+            ],
+        ]);
+        \App\Setting::create([
+            'name' => 'company',
+            'value' => [
+                'name' => 'SHOP 32',
+                'phone' => '',
+                'email' => '',
+            ],
+        ]);
+        \App\Setting::create([
+            'name' => 'social',
+            'value' => [
+                'facebook' => '',
+            ],
+        ]);
+        \App\Setting::create([
+            'name' => 'related_products',
+            'value' => [
+                'rows' => 3,
+                'cols' => 5,
+            ],
+        ]);
+        \App\Setting::create([
+            'name' => 'products_page',
+            'value' => [
+                'rows' => 3,
+                'cols' => 5,
+            ],
+        ]);
+        \App\Setting::create([
+            'name' => 'call_for_order',
+            'value' => '',
+        ]);
     }
 }
