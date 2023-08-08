7e86f1101e2ed1f34da17192c6040f6a9c276b13
Sumon Ahmed <75123992+bdsumon4u@users.noreply.github.com>
2023-08-08 17:17:19 +0600

    Update master.blade.php SearchBar Color

--- resources/views/layouts/yellow/master.blade.php
+++ resources/views/layouts/yellow/master.blade.php
@@ -103,7 +103,7 @@
         .aa-input-container {
             width: 100%;
             height: 100%;
-            border: 3px solid #53a846;
+            border: 3px solid black;
             border-radius: 7px;
         }
         .algolia-autocomplete {
@@ -130,7 +130,7 @@
             box-shadow: none;
         }
         .mobile-header__search-button {
-            background: #64c036;
+            background: red;
             color: white;
             fill: white;
         }
